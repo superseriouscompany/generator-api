967f3900216989d13ce60f52e6c4a03d64a04c7e
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-07-06 18:02:46 -0400

    Add template files from hello.superserious.co

---
+++ generators/app/templates/README.md
@@ -0,0 +1,18 @@
+# <%= app %>
+
+## Installation
+
+    $ npm install
+
+## Running
+
+    $ npm start
+
+## Testing
+
+    $ npm test
+
+## Deploy
+
+    $ ./henoku.sh # run once
+    $ git push gcloud master
--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,1 +1,31 @@
-console.log('great')
+const express    = require('express')
+const bodyParser = require('body-parser')
+const app        = express()
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.disable('etag')
+
+const normalizedPath = require("path").join(__dirname, "routes");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+  require("./routes/" + file)(app);
+});
+
+app.use(function(err, req, res, next) {
+  console.error({err: err, message: err.message, errName: err.name, stack: err.stack}, 'Uncaught server error');
+  res.status(500).json({message: 'Something went wrong.'});
+})
+
+if( process.env.NODE_ENV != 'production' && module.parent ) {
+  module.exports = function(port) {
+    const ref    = app.listen(port);
+    const handle = ref.close.bind(ref);
+    return handle;
+  }
+  return;
+}
+
+app.listen(port, function() {
+  console.log(`Listening on ${port}...`)
+})
